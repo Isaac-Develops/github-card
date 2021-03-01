3964f333305a409166e41a8b01abd050f519335b
GuitarGuyB <isaacben822@gmail.com>
2021-03-01 15:02:37 -0500

    Added github card with user info along with toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Card, Button } from "react-bootstrap"
+import "./App.css"
 
 function App() {
+  const [user, setUser] = useState()
+  const [active, setActive] = useState(false)
+
+  const handleToggle = () => {
+    fetch("https://api.github.com/users/Isaac-Develops")
+      .then((res) => res.json())
+      .then((user) => setUser(user))
+
+    setActive((active) => !active)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Button onClick={handleToggle}>Toggle Profile</Button>
+      {user && active && (
+        <Card>
+          <Card.Img
+            src={user.avatar_url}
+            alt={user.name + "'s Profile Picture"}
+            variant="top"
+          />
+          <Card.Body>
+            <Card.Title>{user.name && user.name}</Card.Title>
+            <Card.Text>
+              {user.followers && "Followers: " + user.followers}
+            </Card.Text>
+            <Card.Text>
+              {user.following && "Following: " + user.following}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
